e21ec3e0b963db9ce316327b51a379f8e161a0cb
Hyukpyo Hong <hyukpyohong@gmail.com>
2017-03-30 23:29:36 -0400

    Fetch Forecast and yesterday's weather from API Still need to contorl asynchronous value pass.

--- src/ZeroZika.js
+++ src/ZeroZika.js
@@ -15,7 +15,30 @@ import InformationScreen from './screens/InformationScreen'
 import SettingScreen from './screens/SettingScreen'
 import TipnTossScreen from './screens/TipntossScreen'
 import Style from './Style';
-import GeolocationExample from './model/getLocation'
+
+
+
+
+function getYesterday(yesterday) {
+    var today = new Date();
+    var yyyy = today.getFullYear();
+    var mm = today.getMonth() + 1; //January is 0!
+
+    if (yesterday) {
+        var dd = today.getDate() - 1;
+    } else {
+        var dd = today.getDate();
+    }
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    return yyyy + '-' + mm + '-' + dd;
+
+}
+
 
 class BackgroundImage extends Component {
     render() {
@@ -30,7 +53,6 @@ class BackgroundImage extends Component {
 
 
 class HomeScreen extends Component {
-
     constructor(props) {
         super(props);
         this.state = { latitude: null, longitude: null };
@@ -40,16 +62,35 @@ class HomeScreen extends Component {
             (position) => {
                 this.state.latitude = position.coords.latitude;
                 this.state.longitude = position.coords.longitude;
+
+                var key = '&key=d05c52f28c5e48d2afc4284807540cb2'
+                var startdate = getYesterday(true);
+                var enddate = getYesterday(false);
+                var forecasturl = 'https://api.weatherbit.io/v1.0/forecast/3hourly?lat=' + this.state.latitude + '&lon=' + this.state.longitude + key;
+                var yesterdayurl = 'https://api.weatherbit.io/v1.0/history?lat=' + this.state.latitude + '&lon=' + this.state.longitude + key + '&start_date=' + startdate + '&end_date=' + enddate + '&key=' + key;
+
+                fetch(forecasturl).then((response) => response.json())
+                    .then((responseJson) => {
+                        console.log("Forecast");
+                        console.log(responseJson);
+                    }).done();
+
+                fetch(yesterdayurl).then((response) => response.json())
+                    .then((responseJson) => {
+                        console.log("Yesterday");
+                        console.log(responseJson);
+                    }).done();
             },
             // This line makes geolocation not working. But looks like neccessary.
             //  (error) => this.setState({ error: error.message }),
             //  { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-        )
+        );
     }
 
     componentWillMount() {
 
     }
+
 
     static navigationOptions = {
         header: {
@@ -61,7 +102,6 @@ class HomeScreen extends Component {
         const { navigate } = this.props.navigation;
         let buttonColor = '#6495ed';
 
-        console.log(this.state.latitude);
         return (
 
             <BackgroundImage>
